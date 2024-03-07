2b274af3103582ec5e2d032c7a02c0ba5e03d1e2
pratik-hanmante <pratikhanmante3@gmail.com>
2024-03-08 03:27:46 +0530

    created signin route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,20 @@ import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
     <BrowserRouter>
     <Header />
+    
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/sign-in" element={<SignIn />}/>
         <Route path="/sign-up" element={<SignUp />}/>
         <Route path="dashboard" element={<Dashboard />}/>
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
