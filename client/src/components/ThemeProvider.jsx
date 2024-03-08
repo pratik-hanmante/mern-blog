dde3b7f0237206968f3bca1afb2d75f7b0f0b9a1
pratik-hanmante <pratikhanmante3@gmail.com>
2024-03-08 23:34:46 +0530

    fixed bugs related to the reducer

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,19 +1,12 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { useSelector } from 'react-redux';
 
-const initialState = {
-    theme: 'light',
-};
-
-const themeSlice = createSlice({
-    name: 'theme',
-    initialState,
-    reducers: {
-        toggleTheme: (state) => {
-            state.theme = state.theme === 'light' ? 'dark' : 'light';
-        },
-        }
-});
-
-export const {toggleTheme} = themeSlice.actions;
-
-export default themeSlice.reducer;
+export default function ThemeProvider({ children }) {
+  const { theme } = useSelector((state) => state.theme);
+  return (
+    <div className={theme}>
+      <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-[rgb(16,23,42)] min-h-screen'>
+        {children}
+      </div>
+    </div>
+  );
+}
