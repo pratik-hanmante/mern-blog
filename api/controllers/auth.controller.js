21330daadd03c3f544e7eb37a457197c2d301489
pratik-hanmante <84140896+pratik-hanmante@users.noreply.github.com>
2025-01-16 23:46:09 +0530

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -37,7 +37,7 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password || email === '' || password === '') {
-    next(errorHandler(400, 'All fields are required'));
+    next(errorHandler(400, 'All fields are mandatory'));
   }
 
   try {
